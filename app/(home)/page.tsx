c2508ef61d72ceb2bdca7ba456b830b579bdc184
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-09-20 18:01:25 -0300

    fix: fix the deployment error

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import LoginSection from "../components/login/login-section";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
